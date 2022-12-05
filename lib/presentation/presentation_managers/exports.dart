915237ce67d8c6e65bfc6dc294845b13acead028
Nasrallah <nsrallahh333@gmail.com>
2022-12-05 22:16:05 +0200

    add new presentation managers

---
+++ lib/presentation/presentation_managers/exports.dart
@@ -0,0 +1,2 @@
+export 'package:advanced_app/presentation/presentation_managers/string_manager.dart';
+export 'package:advanced_app/presentation/views/splash/splash_views.dart';
---
+++ lib/presentation/presentation_managers/routes_manager.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:advanced_app/presentation/presentation_managers/exports.dart';
+
+class Routes {
+  static const String splashRoute = '/';
+  static const String loginRoute = '/login';
+  static const String registerRoute = '/register';
+  static const String forgotPasswordRoute = '/forgotPasswor';
+  static const String mainRoute = '/main';
+  static const String storeDetailsRoute = '/storeDetails';
+}
+
+class RoutesGenerator {
+  static Route<dynamic> getRoutes(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+              appBar: AppBar(
+                title: const Text(AppStrings.noRouteFound),
+              ),
+              body: const Center(child: Text(AppStrings.noRouteFound)),
+            ));
+  }
+}
